72ab7d30bdb059e5519d790c30fd6935dff7202e
Artur Oliveira Gomes <>
2022-07-20 10:34:30 -0400

    feat: :sparkles: connecting using axios and service methods

---
+++ src/services/StudentDataService.ts
@@ -0,0 +1,25 @@
+import http from "../http-common";
+class StudentDataService {
+  getAll() {
+    return http.get("/student");
+  }
+  get(id:string) {
+    return http.get(`/student/${id}`);
+  }
+  create(data:{name:string,email:string}) {
+    return http.post("/student/create", data);
+  }
+  update(id:string, data:{name:string,email:string}) {
+    return http.post(`/student/update/${id}`, data);
+  }
+  delete(id:string) {
+    return http.delete(`/student/${id}`);
+  }
+  deleteAll() {
+    return http.delete(`/student`);
+  }
+  findByName(name:string) {
+    return http.get(`/student?name=${name}`);
+  }
+}
+export default new StudentDataService();
