9930cc581a95cb61d30547c5c35c80ce912c1ba4
Artur Oliveira Gomes <>
2022-07-24 15:05:26 -0400

    refactor: :recycle: picking up correct item to delete

--- src/components/ListStudent.vue
+++ src/components/ListStudent.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- top bar for search and storing new student -->
     <div class="top_bar">
-      <div class="top_bar__search"  v-if="students.length> 0">
+      <div class="top_bar__search" v-if="students.length > 0">
         <v-text-field v-model="search" append-icon="mdi-magnify" label="Digite sua busca" single-line hide-details>
         </v-text-field>
       </div>
@@ -15,43 +15,46 @@
     <!-- container with table of stored students  -->
     <!-- it wont display the table unless at least one student is registred -->
     <div class="container_students">
-      <div v-if="students.length> 0">
+      <div v-if="students.length > 0">
         <v-data-table :headers="headers" :items="students" :search="search" disable-pagination
-        :hide-default-footer="true">
+          :hide-default-footer="true">
 
-        <!-- including modal for confirming student removal -->
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-icon small class="mr-2" @click="editStudent(item.ra)">mdi-pencil</v-icon>
-          <v-dialog v-model="dialog" max-width="290">
-            <!-- including the bin button -->
-            <template v-slot:activator="{ on, attrs }">
-              <v-icon v-bind="attrs" v-on="on" small @click="dialog = true">mdi-delete</v-icon>
-            </template>
-            <!-- modal itself -->
-            <v-card>
-              <v-card-title class="body-2">
-                Deseja remover esse registro?
-              </v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="gray darken-1" text @click="dialog = false">
-                  Cancelar
-                </v-btn>
-                <v-btn color="gray darken-1" text @click="deleteStudent(item.ra); dialog = false">
-                  Confirmar
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
+          <!-- including modal for confirming student removal -->
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small class="mr-2" @click="editStudent(item.ra)">mdi-pencil</v-icon>
+            
+            <v-dialog v-model="dialog" width="500">
+              <template v-slot:activator="{ on }">
+               <v-icon v-on="on" small @click="openModal(item.ra)">mdi-delete</v-icon>
+              </template>
+              <v-card>
+                <v-card-title class="headline grey lighten-2" primary-title>
+                  Deseja remover esse registro?
+                </v-card-title>
+                <v-divider></v-divider>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="gray darken-1" text @click="dialog = false">
+                    Cancelar
+                  </v-btn>
+                  <v-btn color="gray darken-1" text @click="deleteStudent(currentRA); dialog = false">
+                    Confirmar
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
 
-        </template>
-      </v-data-table>
+          </template>
+        </v-data-table>
       </div>
       <!-- won't display if no student is found -->
-      <div v-else><p>Nenhum registro acadêmico cadastrado</p></div>
-    
+      <div v-else>
+        <p>Nenhum registro acadêmico cadastrado</p>
+      </div>
+
     </div>
     <!-- end of container -->
+
   </div>
 </template>
 
@@ -65,6 +68,7 @@ export default {
       students: [],
       name: "",
       dialog: false, // used for modal
+      currentRA: "",
       headers: [ // fields for the table - Vuetify component
         { text: "Registro Acadêmico", align: "start", sortable: true, value: "ra" },
         { text: "Nome", value: "name", sortable: true },
@@ -108,7 +112,7 @@ export default {
     /**
      * the same component 'student-details' is used for both creating and editing
      * a student record
-     */ 
+     */
     editStudent(ra) {
       this.$router.push({ name: "student-details", params: { ra: ra } });
     },
@@ -116,6 +120,7 @@ export default {
       this.$router.push({ name: "student-details", params: { ra: null } });
     },
     deleteStudent(ra) {
+      console.log(ra)
       StudentDataService.delete(ra)
         .then(() => {
           this.refreshList();
@@ -123,6 +128,11 @@ export default {
         .catch((e) => {
           console.log(e);
         });
+    },
+    openModal(ra) {
+      this.dialog = true;
+      this.currentRA = ra;
+
     },
     getDisplayStudent(student) {
       /**
