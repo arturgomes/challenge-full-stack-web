a616416134a82ccb38c0db07cb8590224af19e7d
Artur Oliveira Gomes <>
2022-07-24 16:42:52 -0400

    docs: :memo: commenting Student.vue

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -75,7 +75,6 @@
 
         </v-form>
         <v-container class="button_actions">
-
           <v-btn color="grey lighten-5" small class="mr-2" @click="goBack">
             Cancelar
           </v-btn>
@@ -209,12 +208,14 @@ export default {
       }
     },
     validateCpf(v) {
+      // just checking the right number of digits, but not checking if the CPF is really valid
       return /^([0-9][0-9][0-9])\.([0-9][0-9][0-9])\.([0-9][0-9][0-9])\-([0-9][0-9])$/.test(v)
     },
     validateRa(v) {
       return /^([0-9]+)$/.test(v)
     },
     validateEmail(v) {
+      // Got this from my previous projects.
       return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/.test(v)
     },
 
